177a4b110597b3850248c1c976784e43f51d10ce
shambhu kumar <kshambhu67515@gmail.com>
2025-03-12 16:58:06 +0530

    fix: create separate i2c header and source files

---
+++ project/include/stm32f407vgt_i2c.h
@@ -0,0 +1,38 @@
+
+#ifndef STM_I2C_H
+#define STM_I2C_H
+
+#include "stm32f407xx.h"
+
+
+#define	TRUE	1U
+#define	FALSE	0U
+
+#define	READ	TRUE
+#define	WRITE	FALSE
+
+#define	TX		FALSE
+#define	RX		TRUE
+
+
+
+void i2c1_gpio_config(void);
+void i2c1_config(void);
+void i2c1_enable(void);
+void i2c1_disable(void);
+void i2c1_master_send_start(void);
+void i2c1_master_tx_slave_addr(uint8_t, uint8_t);
+void i2c1_master_tx_data(uint8_t);
+void i2c1_master_tx_stop(void);
+void i2c1_master_rx_data(uint8_t, uint8_t *);
+void i2c1_slave_config(void);
+void i2c1_slave_rx_address(void);
+uint8_t i2c1_slave_rx_data(void);
+void i2c1_slave_tx_data(uint8_t);
+void i2c1_slave_stop(void);
+void i2c1_master_transmit(uint8_t, uint8_t, uint8_t *);
+void i2c1_master_receive(uint8_t, uint8_t, uint8_t *);
+void i2c1_master(uint8_t, uint8_t, uint8_t, uint8_t *);
+
+
+#endif
