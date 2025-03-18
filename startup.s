2656bc39df33b969a4190478f6ad0bc04d8f10ef
shambhu kumar <kshambhu67515@gmail.com>
2025-03-18 19:45:11 +0530

    add(linker.ld): sram section for data add(startup.s): systick handler test: push pop from irq by cpu

--- startup.s
+++ startup.s
@@ -3,10 +3,20 @@
 
 .global reset_handler
 
+@ .equ SYSTICK_CSR,   0xE000E010
+@ .equ SYSTICK_RVR,   0xE000E014
+@ .equ SYSTICK_CVR,   0xE000E018
+@ .equ SYSTICK_CALIB, 0xE000E01C @read only
+
+
 .section .isr_vector
 vector_list:
     .word   0x20000100
     .word   reset_handler
+
+.org 0x0000003C
+    .word systick_handler
+
     .zero   64
 
 
@@ -14,19 +24,32 @@ vector_list:
 .align  1
 .type   reset_handler, %function
 reset_handler:
-    mov r0, #1
-    mov r1, #2
-    mov r2, #3
-    mov r3, #4
+    @ mov only works for data upto 255
+    ldr r0, =0xE000E010 @ SYSTICK_CSR
+    ldr r1, =0xE000E014 @ SYSTICK_RVR
+    ldr r2, =0xE000E018 @ SYSTICK_CVR
+    ldr r3, =0x00ffffff @ timeout
 
-    push {r0-r3}
-    
-    mov r0, #11
-    mov r1, #22
-    mov r2, #33
-    mov r3, #44
+    str r3, [r1] @ set timeout in RVR reg.
+    mov r3, #0
+    str r3, [r2] @ set initial tick value in CVR reg.
+    mov r3, #7
+    str r3, [r0] @ enable systick time in CSR reg.
 
-    pop {r3,r2,r1,r0}
-    
     b .
+
+
+.section .text
+.align  1
+.type   systick_handler, %function
+systick_handler:
+    mov r0, #1;
+    mov r1, #1;
+    mov r2, #1;
+    mov r3, #1;
+
+    @ LR is set to a special value (0xFFFFFFF9), tells the cpu that this is an exception return
+    bx lr
+
+
 
