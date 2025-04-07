8258fd805ea32d9a1ab3a6c8bf5c5ac6f50d5471
shambhu kumar <kshambhu67515@gmail.com>
2025-04-07 20:19:57 +0530

    port FreeRTOS files, working not tested

--- freertos_project/source/main.c
+++ freertos_project/source/main.c
@@ -1,62 +1,146 @@
-/**
- ******************************************************************************
- * @file           : main.c
- * @author         : Shambhu Kumar
- * @brief          : I2C TestCode 
- * @date           : 11/08/2024
- ******************************************************************************
- * @attention
+/*
+ * FreeRTOS V202212.01
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
- * Copyright (c) 2024 CodingBytes.
- * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- ******************************************************************************
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
  */
 
-#include "stm32f407vgt_i2c.h"
+/******************************************************************************
+ * >>>>>> NOTE 1: <<<<<<
+ *
+ * main() can be configured to create either a very simple LED flasher demo, or
+ * a more comprehensive test/demo application.
+ *
+ * To create a very simple LED flasher example, set the
+ * mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY constant (defined below) to 1.  When
+ * this is done, only the standard demo flash tasks are created.  The standard
+ * demo flash example creates three tasks, each of which toggle an LED at a
+ * fixed but different frequency.
+ *
+ * To create a more comprehensive test and demo application, set
+ * mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY to 0.
+ *
+ * >>>>>> NOTE 2: <<<<<<
+ *
+ * In addition to the normal set of standard demo tasks, the comprehensive test
+ * makes heavy use of the floating point unit, and forces floating point
+ * instructions to be used from interrupts that nest three deep.  The nesting
+ * starts from the tick hook function, resulting is an abnormally long context
+ * switch time.  This is done purely to stress test the FPU context switching
+ * implementation, and that part of the test can be removed by setting
+ * configUSE_TICK_HOOK to 0 in FreeRTOSConfig.h.
+ ******************************************************************************
+ *
+ * main() creates all the demo application tasks and software timers, then starts
+ * the scheduler.  The web documentation provides more details of the standard
+ * demo application tasks, which provide no particular functionality, but do
+ * provide a good example of how to use the FreeRTOS API.
+ *
+ * In addition to the standard demo tasks, the following tasks and tests are
+ * defined and/or created within this file:
+ *
+ * "Reg test" tasks - These fill both the core and floating point registers with
+ * known values, then check that each register maintains its expected value for
+ * the lifetime of the task.  Each task uses a different set of values.  The reg
+ * test tasks execute with a very low priority, so get preempted very
+ * frequently.  A register containing an unexpected value is indicative of an
+ * error in the context switching mechanism.
+ *
+ * "Check" timer - The check software timer period is initially set to three
+ * seconds.  The callback function associated with the check software timer
+ * checks that all the standard demo tasks, and the register check tasks, are
+ * not only still executing, but are executing without reporting any errors.  If
+ * the check software timer discovers that a task has either stalled, or
+ * reported an error, then it changes its own execution period from the initial
+ * three seconds, to just 200ms.  The check software timer callback function
+ * also toggles an LED each time it is called.  This provides a visual
+ * indication of the system status:  If the LED toggles every three seconds,
+ * then no issues have been discovered.  If the LED toggles every 200ms, then
+ * an issue has been discovered with at least one task.
+ *
+ * Tick hook - The application tick hook is called from the schedulers tick
+ * interrupt service routine when configUSE_TICK_HOOK is set to 1 in
+ * FreeRTOSConfig.h.  In this example, the tick hook is used to test the kernels
+ * handling of the floating point units (FPU) context, both at the task level
+ * and when nesting interrupts access the floating point unit registers.  The
+ * tick hook function first fills the FPU registers with a known value, it
+ * then triggers a medium priority interrupt.  The medium priority interrupt
+ * fills the FPU registers with a different value, and triggers a high priority
+ * interrupt.  The high priority interrupt once again fills the the FPU
+ * registers with a known value before returning to the medium priority
+ * interrupt.  The medium priority interrupt checks that the FPU registers
+ * contain the values that it wrote to them, then returns to the tick hook
+ * function.  Finally, the tick hook function checks that the FPU registers
+ * contain the values that it wrote to them, before it too returns.
+ *
+ * Button interrupt - The button marked "USER" on the starter kit is used to
+ * demonstrate how to write an interrupt service routine, and how to synchronise
+ * a task with an interrupt.  A task is created that blocks on a test semaphore.
+ * When the USER button is pressed, the button interrupt handler gives the
+ * semaphore, causing the task to unblock.  When the task unblocks, it simply
+ * increments an execution count variable, then returns to block on the
+ * semaphore again.
+ */
 
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "semphr.h"
+
+
+/* Hardware and starter kit includes. */
+#include "stm32f4xx.h"
+#include <misc.h>
 
 /*
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
-*/
+ * Set up the hardware ready to run this demo.
+ */
+static void prvSetupHardware( void );
 
-
-
-
-/// @brief just waste few million clk cycles for generating delay
-/// @param  void
-void temp_delay(void)
-{
-	volatile uint32_t j = 0;
-
-	for(j = 0;j < 2000000;j++)
-	{
-		__asm("nop");	//inline assembly
-	}
-}
-
-
-							/***************  main driver code  ****************/
-
-volatile uint8_t byte[8] = {0};	// store received data
 
 int main(void)
 {
-	i2c1_gpio_config();
-	i2c1_config();
-	i2c1_enable();
+	/* Configure the hardware ready to run the test. */
+	prvSetupHardware();
 
-	//infinte loop
-	for(;;)
-	{
-		i2c1_master(RX,8,7,byte);
-		i2c1_master(TX,8,7,byte);
-		
-		temp_delay();
-	}
+	/* Start the scheduler. */
+	vTaskStartScheduler();
 
-	return 0;
+	/* If all is well, the scheduler will now be running, and the following line
+	will never be reached.  If the following line does execute, then there was
+	insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+	to be created.  See the memory management section on the FreeRTOS web site
+	for more details. */
+	for( ;; );
+}
+
+
+static void prvSetupHardware( void )
+{
+	/* Setup STM32 system (clock, PLL and Flash configuration) */
+	SystemInit();
+
+	/* Ensure all priority bits are assigned as preemption priority bits. */
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
 }
 
