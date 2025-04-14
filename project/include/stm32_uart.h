8312bc0396abc3f283ff390b9e22d98593f47991
shambhu kumar <kshambhu67515@gmail.com>
2025-04-14 18:13:24 +0530

    layout the directory, add some flags to makefile

---
+++ project/include/stm32_uart.h
@@ -0,0 +1,29 @@
+/**
+ * @file stm32_uart.h
+ * @author shambhu kumar
+ * @brief uart header file
+ * @version 0.1
+ * @date 2025-04-14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+
+#ifndef STM32_UART_H
+#define STM32_UART_H
+
+
+#include "stm32f407xx.h"
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ project/source/stm32_uart.c
@@ -0,0 +1,16 @@
+/**
+ * @file stm32_uart.c
+ * @author shambhu kumar
+ * @brief uart source file
+ * @version 0.1
+ * @date 2025-04-14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+
+#include "stm32_uart.h"
+
+
+
