0992b6128b3dc75b916800c93dc8869ce4e0bfb4
shambhu kumar <kshambhu67515@gmail.com>
2025-04-20 19:48:13 +0530

    add rs232 data type for rx buf and tx buf

--- project/include/stm32_uart.h
+++ project/include/stm32_uart.h
@@ -17,7 +17,23 @@
 #include "stm32f407xx.h"
 
 
+#define MAX_BUF_SIZE   25
+#define DISABLE         0
+#define Enable          1
+#define BAUD_RATE       9600
+#define FRAME_SIZE      8
+#define STOP_BIT_SIZE   1
+#define PARITY_BIT      DISABLE
 
+
+typedef struct rs232
+{
+    char rx_buf[MAX_BUF_SIZE + 1];
+    char tx_buf[MAX_BUF_SIZE + 1];
+    uint8_t rx_buf_char_num;
+    uint8_t tx_buf_char_num;
+
+}rs232_obj;
 
 
 
